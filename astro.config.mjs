3ff2f971c83f57d162fb9a441edf325adac7cc6c
eliasrestea <116088233+eliasrestea@users.noreply.github.com>
2023-11-29 21:05:13 +0200

    squash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://eliasrestea.github.io',
+  site: 'https://eliasrestea.github.io/BioCoderDojo/',
   base: '/',
 });
