4dd750015a15e832118ceb8a3749e2606eea2641
eliasrestea <eliasrestea@gmail.com>
2023-11-30 20:55:16 +0200

    Feat: form that saves the data in excel, added footer and slight design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-	'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	'./node_modules/flowbite/**/*.js',
-],
-	theme: {
-		extend: {
-			blur: {
-			  xs: '2px',
-			}
-		  }
-	  
-	},
-	plugins: [
-		require('flowbite/plugin'),
-	],
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        './node_modules/flowbite/**/*.js',
+    ],
+    theme: {
+        extend: {
+            blur: {
+                xs: '2px',
+            }
+        }
+    },
+    plugins: [
+        require('flowbite/plugin'),
+    ],
+    safelist: [
+        'bg-green-200', 'text-green-800', 'bg-red-200', 'text-red-800',
+		'ring-red-400', 'ring-green-400', 'text-green-500', 'text-red-500',
+		'bg-red-400', 'bg-green-400'
+    ],
 }
